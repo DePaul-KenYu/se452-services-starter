60a571cec1866fba6f0fd2dc5424c16da5c8c909
Ken Yu <kyu@cdm.depaul.edu>
2023-08-29 23:24:36 -0500

    updated to include junit test

--- src/main/java/edu/depaul/cdm/se452/concept/services/StudentService.java
+++ src/main/java/edu/depaul/cdm/se452/concept/services/StudentService.java
@@ -1,20 +1,30 @@
 package edu.depaul.cdm.se452.concept.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.depaul.cdm.se452.concept.rdbm.school.simple.Student;
 import edu.depaul.cdm.se452.concept.rdbm.school.simple.StudentRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
@@ -48,11 +58,18 @@ public class StudentService {
         log.traceExit("Exit delete");
     }           
 
-/* 
+    @PostMapping("/valid")
+    @Operation(summary = "Save the student and returns the student id")
+    public ResponseEntity<String> validatedSave(@Valid @RequestBody Student student) {
+        log.traceEntry("enter save", student);
+        repo.save(student);
+        log.traceExit("exit save", student);
+        return ResponseEntity.ok("new id is " + student.getId());
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-      MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -61,6 +78,4 @@ public class StudentService {
         });
         return errors;
     }    
-    */
-
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,10 @@
 insert into course(dept, num) values ('SE', '452');
 insert into course(dept, num) values ('SE', '352');   
+
+insert into students(nm, stu_id, email, age) values ('James Bond', '007', 'james.bond@mi6.gov', 41);
+
+insert into instructor(name) values ('James');
+insert into instructor(name) values ('Ken');
+
+insert into university(name) values ('DePaul');
+insert into university(name) values ('Loyola');
